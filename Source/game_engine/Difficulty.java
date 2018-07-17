8b2093251ab5f2bcb32490334af328305d8f0885
hunterszone <k.drenski91@gmail.com>
2018-07-17 08:21:58 +0300

    fin music no longer looping

--- Source/game_engine/Difficulty.java
+++ Source/game_engine/Difficulty.java
@@ -9,149 +9,157 @@ import sound_engine.LoadSounds;
 
 public class Difficulty {
 
- public static void restart() {
+	public static void restart(){
+		
+		DrawScene.finMusicIsPlayed = false;
+		
+		if(InitObjects.god == true){
+			InitObjects.god = false;
+		}
+				
+	    DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
+        InitObjects.ingame = true;
+        UpdateObjects.lifeEvilHead = 3;
+        UpdateObjects.lifeMyShip = 3;
+        UpdateObjects.lifeBunker = 3;
+        
+        MyShip.myShip = new MyShip(40, 180);
+        MyShip.myShip.isVisible();
+        
+        EvilHead.evilHead = new EvilHead(640, 180);
+        EvilHead.evilHead.isVisible();
+        EvilHead.evilHead.AIOnEasy();
+        
+        Bunker.bunkerObj = new Bunker(450, 680);
+        Bunker.bunkerObj.isVisible();
+        
+        VolBtn.volButt = new VolBtn(940, 15);
+        VolBtn.volButt.isVisible();
 
-  if (InitObjects.god == true) {
-   InitObjects.god = false;
-  }
+		InitObjects.initAliens();
+		InitObjects.initGold();
+		InitObjects.initDragons();
+		InitObjects.initHealth();
+		
+        InitObjects.timerHard.stop();
+        InitObjects.timerMedium.stop();
+        InitObjects.timerEasy.restart();
+        LoadSounds.gameWon.play();
+        LoadSounds.gameLost.stop();
+        LoadSounds.bgMusic.loop();
+        LoadSounds.roar.stop();		
+	}
+	
+	public static void easy(){
+		
+		DrawScene.finMusicIsPlayed = false;
+			
+		DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
+		InitObjects.ingame = true;
+		UpdateObjects.lifeEvilHead = 3;
+		UpdateObjects.lifeMyShip = 3;
+		UpdateObjects.lifeBunker = 3;
+        
+		MyShip.myShip = new MyShip(40, 180);
+		MyShip.myShip.isVisible();
+        
+		EvilHead.evilHead = new EvilHead(640, 180);
+		EvilHead.evilHead.isVisible();
+		EvilHead.evilHead.AIOnMedium();
+		
+		Bunker.bunkerObj = new Bunker(450, 680);
+		Bunker.bunkerObj.isVisible();
+        
+		VolBtn.volButt = new VolBtn(940, 15);
+		VolBtn.volButt.isVisible();
 
-  DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
-  InitObjects.ingame = true;
-  UpdateObjects.lifeEvilHead = 3;
-  UpdateObjects.lifeMyShip = 3;
-  UpdateObjects.lifeBunker = 3;
+		InitObjects.initAliens();
+		InitObjects.initGold();
+		InitObjects.initDragons();
+		InitObjects.initHealth();
+		
+		InitObjects.timerMedium.stop();
+		InitObjects.timerHard.stop();
+		InitObjects.timerEasy.restart();
+		LoadSounds.gameWon.play();
+		LoadSounds.gameLost.stop();
+		LoadSounds.bgMusic.loop();
+		LoadSounds.roar.stop();
+	}
+	
+	public static void medium(){
+		
+		DrawScene.finMusicIsPlayed = false;
+		
+		DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
+		InitObjects.ingame = true;
+		UpdateObjects.lifeEvilHead = 3;
+		UpdateObjects.lifeMyShip = 3;
+		UpdateObjects.lifeBunker = 3;
+        
+		MyShip.myShip = new MyShip(40, 180);
+		MyShip.myShip.isVisible();
+        
+		EvilHead.evilHead = new EvilHead(640, 180);
+		EvilHead.evilHead.isVisible();
+		EvilHead.evilHead.AIOnMedium();
+		
+		Bunker.bunkerObj = new Bunker(450, 680);
+		Bunker.bunkerObj.isVisible();
+        
+		VolBtn.volButt = new VolBtn(940, 15);
+		VolBtn.volButt.isVisible();
 
-  MyShip.myShip = new MyShip(40, 180);
-  MyShip.myShip.isVisible();
+		InitObjects.initAliens();
+		InitObjects.initGold();
+		InitObjects.initDragons();
+		InitObjects.initHealth();
+		
+		InitObjects.timerEasy.stop();
+		InitObjects.timerHard.stop();;
+		InitObjects.timerMedium.restart();
+		
+		LoadSounds.gameWon.play();
+		LoadSounds.gameLost.stop();
+		LoadSounds.bgMusic.loop();
+		LoadSounds.roar.stop();
+	}
+	
+	public static void hard(){
+		
+		DrawScene.finMusicIsPlayed = false;
+		
+		DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
+		InitObjects.ingame = true;
+		UpdateObjects.lifeEvilHead = 3;
+		UpdateObjects.lifeMyShip = 3;
+		UpdateObjects.lifeBunker = 3;
+		
+		MyShip.myShip = new MyShip(40, 180);
+		MyShip.myShip.isVisible();
+        
+		EvilHead.evilHead = new EvilHead(640, 180);
+		EvilHead.evilHead.isVisible();
+		EvilHead.evilHead.AIOnMedium();
+		
+		Bunker.bunkerObj = new Bunker(450, 680);
+		Bunker.bunkerObj.isVisible();
+        
+		VolBtn.volButt = new VolBtn(940, 15);
+		VolBtn.volButt.isVisible();
 
-  EvilHead.evilHead = new EvilHead(640, 180);
-  EvilHead.evilHead.isVisible();
-  EvilHead.evilHead.AIOnEasy();
-
-  Bunker.bunkerObj = new Bunker(450, 680);
-  Bunker.bunkerObj.isVisible();
-
-  VolBtn.volButt = new VolBtn(940, 15);
-  VolBtn.volButt.isVisible();
-
-  InitObjects.initAliens();
-  InitObjects.initGold();
-  InitObjects.initDragons();
-  InitObjects.initHealth();
-
-  InitObjects.timerHard.stop();
-  InitObjects.timerMedium.stop();
-  InitObjects.timerEasy.restart();
-  LoadSounds.gameWon.play();
-  LoadSounds.gameLost.stop();
-  LoadSounds.bgMusic.loop();
-  LoadSounds.roar.stop();
- }
-
- public static void easy() {
-
-  DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
-  InitObjects.ingame = true;
-  UpdateObjects.lifeEvilHead = 3;
-  UpdateObjects.lifeMyShip = 3;
-  UpdateObjects.lifeBunker = 3;
-
-  MyShip.myShip = new MyShip(40, 180);
-  MyShip.myShip.isVisible();
-
-  EvilHead.evilHead = new EvilHead(640, 180);
-  EvilHead.evilHead.isVisible();
-  EvilHead.evilHead.AIOnMedium();
-
-  Bunker.bunkerObj = new Bunker(450, 680);
-  Bunker.bunkerObj.isVisible();
-
-  VolBtn.volButt = new VolBtn(940, 15);
-  VolBtn.volButt.isVisible();
-
-  InitObjects.initAliens();
-  InitObjects.initGold();
-  InitObjects.initDragons();
-  InitObjects.initHealth();
-
-  InitObjects.timerMedium.stop();
-  InitObjects.timerHard.stop();;
-  InitObjects.timerEasy.restart();
-  LoadSounds.gameWon.play();
-  LoadSounds.gameLost.stop();
-  LoadSounds.bgMusic.loop();
-  LoadSounds.roar.stop();
- }
-
- public static void medium() {
-
-  DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
-  InitObjects.ingame = true;
-  UpdateObjects.lifeEvilHead = 3;
-  UpdateObjects.lifeMyShip = 3;
-  UpdateObjects.lifeBunker = 3;
-
-  MyShip.myShip = new MyShip(40, 180);
-  MyShip.myShip.isVisible();
-
-  EvilHead.evilHead = new EvilHead(640, 180);
-  EvilHead.evilHead.isVisible();
-  EvilHead.evilHead.AIOnMedium();
-
-  Bunker.bunkerObj = new Bunker(450, 680);
-  Bunker.bunkerObj.isVisible();
-
-  VolBtn.volButt = new VolBtn(940, 15);
-  VolBtn.volButt.isVisible();
-
-  InitObjects.initAliens();
-  InitObjects.initGold();
-  InitObjects.initDragons();
-  InitObjects.initHealth();
-
-  InitObjects.timerEasy.stop();
-  InitObjects.timerHard.stop();;
-  InitObjects.timerMedium.restart();
-
-  LoadSounds.gameWon.play();
-  LoadSounds.gameLost.stop();
-  LoadSounds.bgMusic.loop();
-  LoadSounds.roar.stop();
- }
-
- public static void hard() {
-
-  DrawScene.bg1 = Toolkit.getDefaultToolkit().createImage("images/tenor.gif");
-  InitObjects.ingame = true;
-  UpdateObjects.lifeEvilHead = 3;
-  UpdateObjects.lifeMyShip = 3;
-  UpdateObjects.lifeBunker = 3;
-
-  MyShip.myShip = new MyShip(40, 180);
-  MyShip.myShip.isVisible();
-
-  EvilHead.evilHead = new EvilHead(640, 180);
-  EvilHead.evilHead.isVisible();
-  EvilHead.evilHead.AIOnMedium();
-
-  Bunker.bunkerObj = new Bunker(450, 680);
-  Bunker.bunkerObj.isVisible();
-
-  VolBtn.volButt = new VolBtn(940, 15);
-  VolBtn.volButt.isVisible();
-
-  InitObjects.initAliens();
-  InitObjects.initGold();
-  InitObjects.initDragons();
-  InitObjects.initHealth();
-
-  InitObjects.timerEasy.stop();
-  InitObjects.timerMedium.stop();;
-  InitObjects.timerHard.restart();
-  LoadSounds.gameWon.play();
-  LoadSounds.gameLost.stop();
-  LoadSounds.bgMusic.loop();
-  LoadSounds.roar.stop();
- }
+		InitObjects.initAliens();
+		InitObjects.initGold();
+		InitObjects.initDragons();
+		InitObjects.initHealth();
+        
+		InitObjects.timerEasy.stop();
+		InitObjects.timerMedium.stop();;
+		InitObjects.timerHard.restart();
+		LoadSounds.gameWon.play();
+		LoadSounds.gameLost.stop();
+		LoadSounds.bgMusic.loop();
+		LoadSounds.roar.stop();
+	}
 
 }
